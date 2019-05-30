b846d92d72eb45e2bf13a8609a86efe4bd09885c
Orane Jecker <jecker@mathicsepc65.epfl.ch>
2019-05-30 09:47:34 +0200

    29.05

--- Chapitre 1 - Systemes equations lineaires/AL_Fct.py
+++ Chapitre 1 - Systemes equations lineaires/AL_Fct.py
@@ -6,7 +6,6 @@ Created on Wed Mar 13 16:42:29 2019
 @author: jecker
 """
 from __future__ import division
-
 import numpy as np
 from IPython.display import display, Latex
 import matplotlib.pyplot as plt
@@ -28,10 +27,10 @@ def bgc(color, cell=None):
     ).format(color)
 
     display(HTML('<img src onerror="{}">'.format(script)))
+###############################################################################
 
+## PRINTS Equations, systems, matrix
 
-
-#PRINTS Equations, systems, matrix
 def strEq(n,coeff):# Latex str of one equation in format a1x1+ ...+anxn or with coefficients.
     Eq=''
     if str(coeff)=='' or coeff==[]: 
@@ -60,14 +59,14 @@ def printEq(n,coeff,b):# Latex Print of one equation in format a1x1+ ...+anxn or
     texEq=texEq + '$'
     display(Latex(texEq))
     
-def printSyst(m,n,MatCoeff,b):# Latex Print of one system of m equation in format ai1x1+ ...+ainxn=bi or with coeff in MatCoeff.
-    if (str(MatCoeff)=='') or (len(MatCoeff[1])==n and len(MatCoeff)==m):  #ensures that MatCoeff has proper dimensions 
+def printSyst(m,n,A,b):# Latex Print of one system of m equation in format ai1x1+ ...+ainxn=bi or with coeff in MatCoeff.
+    if (str(A)=='') or (len(A[1])==n and len(A)==m):  #ensures that MatCoeff has proper dimensions 
         texSyst='$\\begin{cases}'
         Eq_list=[]
-        MatCoeff = [MatCoeff[i]+[b[i]]for i in range(0,m)]
-        MatCoeff=np.array(MatCoeff) #just in case it's not
+        A = [A[i]+[b[i]]for i in range(0,m)] #becomes augmented matrix
+        A=np.array(A) #just in case it's not
         for i in range(m):          
-            if str(MatCoeff)=='':
+            if str(A)=='':
                 Eq_i=''
                 if n==1:
                     Eq_i=Eq_i + 'a_{' + str(i+1) + '1}' + 'x_1 = b_' + str(i+1)
@@ -76,7 +75,7 @@ def printSyst(m,n,MatCoeff,b):# Latex Print of one system of m equation in forma
                 else:
                     Eq_i=Eq_i + 'a_{' + str(i+1) + '1}' + 'x_1 + \ldots +' + 'a_{' +  str(i+1) +str(n) + '}' + 'x_'+ str(n) + '=b_'  + str(i+1)
             else:
-                Eq_i=strEq(n,MatCoeff[i,:])
+                Eq_i=strEq(n,A[i,:])
             Eq_list.append(Eq_i)
             texSyst=texSyst+  Eq_list[i] + '\\\\'
         texSyst =texSyst+ '\\end{cases}$'
@@ -85,7 +84,8 @@ def printSyst(m,n,MatCoeff,b):# Latex Print of one system of m equation in forma
         print("La matrice des coefficients n'a pas les bonnes dimensions")
 
 
-def texMatrix(A): #return tex expression of one matrix
+def texMatrix(A): #return tex expression of one matrix of coefficients
+   
     texApre ='\\left(\\begin{array}{'
     texA = ''
     for i in np.asarray(A) :
@@ -102,8 +102,40 @@ def texMatrix(A): #return tex expression of one matrix
     texA = texApre + '}  ' + texA[:-2] + ' \\end{array}\\right)'
     return texA
 
+
+def texMatrixAug(A,b): #return tex expression of one matrix (A|b) where b can also be a matrix
+    m=len(A[0])
+    A=np.concatenate((A,b), axis=1)
+    texApre ='\\left(\\begin{array}{'
+    texA = ''
+    for i in np.asarray(A) :
+        texALigne = ''
+        texALigne = texALigne + str(round(i[0],3) if i[0] %1 else int(i[0]))
+        if texA == '' :
+            texApre = texApre + 'c'
+        for j in i[1:m] :
+            if texA == '' :
+                texApre = texApre + 'c'
+            texALigne = texALigne + ' & ' + str(round(j,3) if j %1 else int(j))
+        if texA == '' :
+            texApre = texApre + '| c'
+        for j in i[m:] :   
+            if texA == '' :
+                texApre = texApre + 'c'
+            texALigne = texALigne + ' & ' + str(round(j,3) if j %1 else int(j))
+        texALigne = texALigne + ' \\\\'
+        texA = texA + texALigne
+    texA = texApre + '}  ' + texA[:-2] + ' \\end{array}\\right)'
+    return texA
+
 def printA(A) : #Print matrix 
     texA='$'+ texMatrix(A) + '$'
+    # print(texA)
+    display(Latex(texA))  
+
+
+def printAAug(A,b) : #Print matrix (A|b)
+    texA='$'+ texMatrixAug(A,b) + '$'
     # print(texA)
     display(Latex(texA))  
     
@@ -111,6 +143,13 @@ def printEquMatrices(listOfMatrices): #list of matrices is M=[M1, M2, ..., Mn]
     texEqu='$' + texMatrix(listOfMatrices[0])
     for i in range(1,len(listOfMatrices)):
         texEqu=texEqu+ '\\quad \\sim \\quad' + texMatrix(listOfMatrices[i])
+    texEqu=texEqu+ '$'
+    display(Latex(texEqu))
+    
+def printEquMatricesAug(listOfMatrices, listOfRhS): #list of matrices is M=[M1, M2, ..., Mn] where Mi=(Mi|b) 
+    texEqu='$' + texMatrixAug(listOfMatrices[0],listOfRhS[0])
+    for i in range(1,len(listOfMatrices)):
+        texEqu=texEqu+ '\\quad \\sim \\quad' + texMatrixAug(listOfMatrices[i], listOfRhS[i])
     texEqu=texEqu+ '$'
     display(Latex(texEqu))
 
@@ -156,34 +195,35 @@ def SolOfEq(sol,coeff, i):
         print("La suite entrée n'est pas une solution de l'équation",i)
     return abs(np.dot(A,sol)-coeff[len(coeff)-1])<1e-10
 
-def SolOfSyst(solution,MatriceCoeff,b):
-    MatriceCoeff = [MatriceCoeff[i]+[b[i]] for i in range(0,len(MatriceCoeff))]
-    MatriceCoeff=np.array(MatriceCoeff)
-    isSol=[SolOfEq(solution, MatriceCoeff[i,:],i+1) for i in range(0,len(MatriceCoeff))]
+def SolOfSyst(solution,A,b):
+    A = [A[i]+[b[i]] for i in range(0,len(A))]
+    A=np.array(A)
+    isSol=[SolOfEq(solution, A[i,:],i+1) for i in range(0,len(A))]
     if all(isSol[i]==True for i in range(len(isSol))):
         print("C'est une solution du système")
     else:
         print("Ce n'est pas une solution du système")
         
 #%%Plots using plotly
-def Plot2DSys(xL,xR,p,MatCoeff,b): # small values for p allows for dots to be seen
-        MatCoeff = [MatCoeff[i]+[b[i]] for i in range(0,len(MatCoeff))]
-        MatCoeff=np.array(MatCoeff)
+def Plot2DSys(xL,xR,p,A,b): # small values for p allows for dots to be seen
+        A = [A[i]+[b[i]] for i in range(0,len(A))]
+        A=np.array(A)
         t=np.linspace(xL,xR,p)
         legend=[]
         data=[]
-        for i in range(1,len(MatCoeff)+1):
-            if (abs(MatCoeff[i-1,1])) > abs (MatCoeff[i-1,0]) :
-                trace=go.Scatter(x=t,  y= (MatCoeff[i-1,2]-MatCoeff[i-1,0]*t)/MatCoeff[i-1,1], name='Droite %d'%i)
+        for i in range(1,len(A)+1):
+            if (abs(A[i-1,1])) > abs (A[i-1,0]) :
+                trace=go.Scatter(x=t,  y= (A[i-1,2]-A[i-1,0]*t)/A[i-1,1], name='Droite %d'%i)
             else :
-                trace=go.Scatter(x=(MatCoeff[i-1,2]-MatCoeff[i-1,1]*t)/MatCoeff[i-1,0],  y= t, name='Droite %d'%i)
+                trace=go.Scatter(x=(A[i-1,2]-A[i-1,1]*t)/A[i-1,0],  y= t, name='Droite %d'%i)
             data.append(trace)
         fig = go.Figure(data=data)
         plotly.offline.iplot(fig)
-      
-def Plot3DSys(xL,xR,p,MatCoeff,b): # small values for p allows for dots to be seen
-    MatCoeff = [MatCoeff[i]+[b[i]] for i in range(0,len(MatCoeff))]
-    MatCoeff=np.array(MatCoeff)
+
+        
+def Plot3DSys(xL,xR,p,A,b): # small values for p allows for dots to be seen
+    A = [A[i]+[b[i]] for i in range(0,len(A))]
+    A=np.array(A)
     gr='rgb(102,255,102)'
     org='rgb(255,117,26)'
     red= 'rgb(255,0,0)'
@@ -193,7 +233,7 @@ def Plot3DSys(xL,xR,p,MatCoeff,b): # small values for p allows for dots to be se
     t = np.linspace(xL, xR, p)
     tGrid, sGrid = np.meshgrid(s, t)
     data=[]
-    for i in range(0, len(MatCoeff)):
+    for i in range(0, len(A)):
         colorscale=[[0.0,colors[i]],
                [0.1, colors[i]],
                [0.2, colors[i]],
@@ -206,20 +246,20 @@ def Plot3DSys(xL,xR,p,MatCoeff,b): # small values for p allows for dots to be se
                [0.9, colors[i]],
                [1.0, colors[i]]]
         j=i+1 
-        if (abs(MatCoeff[i,2])) > abs (MatCoeff[i,1]) : # z en fonction de x,y
+        if (abs(A[i,2])) > abs (A[i,1]) : # z en fonction de x,y
             x = sGrid
             y = tGrid
-            surface = go.Surface(x=x, y=y, z=(MatCoeff[i,3]-MatCoeff[i,0]*x -MatCoeff[i,1]*y)/MatCoeff[i,2],
+            surface = go.Surface(x=x, y=y, z=(A[i,3]-A[i,0]*x -A[i,1]*y)/A[i,2],
                              showscale=False, colorscale=colorscale, opacity=1, name='Plan %d' %j)
-        elif (MatCoeff[i,2]==0 and MatCoeff[i,1]==0): # x =b
+        elif (A[i,2]==0 and A[i,1]==0): # x =b
             y = sGrid
             z = tGrid
-            surface = go.Surface(x=MatCoeff[i,3]-MatCoeff[i,1]*y, y=y, z=z,
+            surface = go.Surface(x=A[i,3]-A[i,1]*y, y=y, z=z,
                              showscale=False, colorscale=colorscale, opacity=1, name='Plan %d' %j)
         else:# y en fonction de x,z
             x = sGrid
             z = tGrid
-            surface = go.Surface(x=x, y=(MatCoeff[i,3]-MatCoeff[i,0]*x -MatCoeff[i,2]*z)/MatCoeff[i,1], z=z,
+            surface = go.Surface(x=x, y=(A[i,3]-A[i,0]*x -A[i,2]*z)/A[i,1], z=z,
                              showscale=False, colorscale=colorscale, opacity=1, name='Plan %d' %j)
             
         data.append(surface)
@@ -263,22 +303,24 @@ def echZero(indice, M): #echelonne la matrice pour mettre les zeros dans les lig
     return Mat
 
 def Eij(M, i,j): #matrice elementaire, echange la ligne i avec la ligne j
+    M=np.array(M)
     M[[i,j],:]=M[[j,i],:]
     return M
 
 def Ealpha(M, i, alpha): # matrice elementaire, multiple la ligne i par le scalaire alpha
+    M=np.array(M)
     M[i,:]=alpha*M[i,:]
     return M
 
 def Eijalpha(M, i,j, alpha): # matrice elementaire, AJOUTE à la ligne i alpha *ligne j. Attention alpha + ou -
+    M=np.array(M)
     M[i,:]=M[i,:] +  alpha *M[j,:]
     return M 
 
-def echelonMat(MatCoeff,b):  #Nous donne la matrice echelonne d un system [A|b]
-    MatCoeff = [MatCoeff[i]+[b[i]] for i in range(0,len(MatCoeff))]
-    Mat=np.array(MatCoeff)
+def echelonMat(A,b):  #Nous donne la matrice echelonne d un system [A|b]
+    A = [A[i]+[b[i]] for i in range(0,len(A))]
+    Mat=np.array(A)
     Mat=Mat.astype(float) # in case the array in int instead of float.
-
     numPivot=0
     for i in range(len(Mat)):
         j=i
@@ -289,7 +331,7 @@ def echelonMat(MatCoeff,b):  #Nous donne la matrice echelonne d un system [A|b]
             if len(Mat[0,:])>j:
                 Mat[i+1:len(Mat),:]=0
             print("La matrice est sous la forme échelonnée")
-            printEquMatrices([MatCoeff, Mat])
+            printEquMatrices([A, Mat])
             break     
         if abs(Mat[i,j])<1e-15:
                 Mat[i,j]=0
@@ -297,7 +339,7 @@ def echelonMat(MatCoeff,b):  #Nous donne la matrice echelonne d un system [A|b]
                 M= echZero(zero,Mat[i:,:] )
                 Mat[i:,:]=M
         Mat=Ealpha(Mat, i,1/Mat[i,j] ) #normalement Mat[i,j]!=0
-        for k in range(i+1,len(MatCoeff)):
+        for k in range(i+1,len(A)):
             Mat=Eijalpha(Mat, k,i, -Mat[k,j])
             #Mat[k,:]=[0 if abs(Mat[k,l])<1e-15 else Mat[k,l] for l in range(len(MatCoeff[0,:]))]
         numPivot+=1
@@ -305,9 +347,9 @@ def echelonMat(MatCoeff,b):  #Nous donne la matrice echelonne d un system [A|b]
         printA(np.array(Mat)) 
     return np.asmatrix(Mat)
 
-def echelonMatCoeff(MatCoeff): #take echelonMAt but without b. 
-    b= [0 for i in range(len(MatCoeff))]
-    Mat = [MatCoeff[i]+[b[i]] for i in range(0,len(MatCoeff))]
+def echelonMatCoeff(A): #take echelonMAt but without b. 
+    b= [0 for i in range(len(A))]
+    Mat = [A[i]+[b[i]] for i in range(0,len(A))]
     Mat=np.array(Mat)
     Mat=Mat.astype(float) # in case the array in int instead of float.
     numPivot=0
@@ -320,7 +362,7 @@ def echelonMatCoeff(MatCoeff): #take echelonMAt but without b.
             if len(Mat[0,:])>j:
                 Mat[i+1:len(Mat),:]=0
             print("La matrice est sous la forme échelonnée")
-            printEquMatrices([np.asmatrix(MatCoeff), np.asmatrix(Mat[:, :len(MatCoeff[0])])])
+            printEquMatrices([np.asmatrix(A), np.asmatrix(Mat[:, :len(A[0])])])
             break     
         if abs(Mat[i,j])<1e-15:
                 Mat[i,j]=0
@@ -328,18 +370,18 @@ def echelonMatCoeff(MatCoeff): #take echelonMAt but without b.
                 M= echZero(zero,Mat[i:,:] )
                 Mat[i:,:]=M
         Mat=Ealpha(Mat, i,1/Mat[i,j] ) #normalement Mat[i,j]!=0
-        for k in range(i+1,len(MatCoeff)):
+        for k in range(i+1,len(A)):
             Mat=Eijalpha(Mat, k,i, -Mat[k,j])
             #Mat[k,:]=[0 if abs(Mat[k,l])<1e-15 else Mat[k,l] for l in range(len(MatCoeff[0,:]))]
         numPivot+=1
         Mat[abs(Mat)<1e-15]=0
-        printA(np.asmatrix(Mat[:, :len(MatCoeff[0])])) 
+        printA(np.asmatrix(Mat[:, :len(A[0])])) 
     return np.asmatrix(Mat)
 
-def echelonRedMat(MatCoeff, b):
-    Mat=echelonMat(MatCoeff,b)
+def echelonRedMat(A, b):
+    Mat=echelonMat(A,b)
     Mat=np.array(Mat)
-    MatAugm = [MatCoeff[i]+[b[i]] for i in range(0,len(MatCoeff))]
+    MatAugm = [A[i]+[b[i]] for i in range(0,len(A))]
     i=(len(Mat)-1)
     while i>=1:
         while all(abs(Mat[i,:len(Mat[0])-1])<1e-15) and i!=0:#if ligne (or rest of) is zero, take next ligne
@@ -357,5 +399,3 @@ def echelonRedMat(MatCoeff, b):
     printEquMatrices([MatAugm, Mat])
     return Mat
 
-def f(a,x):
-    return x+ a
