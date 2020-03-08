283762e2e7648c476dbe296e45b25f31edda1e59
Riccardo Tenderini <riccardo.tenderini@mail.polimi.it>
2020-03-08 20:38:21 +0100

    Added corrections to exercices of notebooks 2_2 and 2_3 in chapter 2 corrections

--- Chapitre 2 - Algebre matricielle/Corrections/corrections.py
+++ Chapitre 2 - Algebre matricielle/Corrections/corrections.py
@@ -1,12 +1,12 @@
 import sys
 sys.path.insert(0, './../')
-
+import numpy as np
 import plotly
 plotly.offline.init_notebook_mode(connected=True)
 import ipywidgets as widgets
 from IPython.display import display, Latex
 from ipywidgets import interact_manual, Layout
-from Librairie.AL_Fct import printA
+from Librairie.AL_Fct import printA, texMatrix, isDiag, isSym
 
 
 def Ex2Chapitre2_1():
@@ -83,6 +83,224 @@ def Ex3Chapitre2_1():
             printA(A)
         else:
             print("C'est faux.")
+
+    interact_manual(correction, a=a, b=b, c=c, d=d)
+
+    return
+
+
+def Ex1Chapitre2_2():
+    """Provides the correction of exercise 1 of notebook 2_2
+    """
+
+    a = widgets.Checkbox(
+        value=False,
+        description=r'Le produit $A \cdot B$ vaut: <br>'
+                    r'\begin{equation*} \qquad A \cdot B = \begin{bmatrix}-1 & 4\\-3& -3\\2 & 0\end{bmatrix}\end{equation*}',
+        disabled=False,
+        layout=Layout(width='100%', height='110px')
+    )
+    b = widgets.Checkbox(
+        value=False,
+        description=r'Le produit $A \cdot B$ vaut: <br>'
+                    r'\begin{equation*} \qquad A \cdot B =\begin{bmatrix}-1 & 8\\-3& 3\\-2 & 4\end{bmatrix}\end{equation*}',
+        disabled=False,
+        layout=Layout(width='100%', height='110px')
+
+    )
+    c = widgets.Checkbox(
+        value=False,
+        description=r'Le produit $A \cdot B$ vaut: <br>'
+                    r'\begin{equation*} \qquad A \cdot B =\begin{bmatrix}5 & -4\\1 & 0\end{bmatrix}\end{equation*}',
+        disabled=False,
+        layout=Layout(width='100%', height='90px')
+    )
+    d = widgets.Checkbox(
+        value=False,
+        description=r"Le produit $A \cdot B$ n'est pas définie",
+        disabled=False,
+        layout=Layout(width='100%', height='60px')
+    )
+
+    def correction(a, b, c, d):
+        if b and not a and not c and not d:
+            print("C'est correct!")
+        else:
+            print("C'est faux.")
+
+    interact_manual(correction, a=a, b=b, c=c, d=d)
+
+    return
+
+
+def Ex2Chapitre2_2():
+    """Provides the correction of exercise 2 of notebook 2_2
+    """
+
+    a = widgets.Checkbox(
+        value=False,
+        description=r'Le produit $A \cdot B$ appartient à $M_{3 \times 3}(\mathbb{R})$',
+        disabled=False,
+        layout=Layout(width='80%', height='50px')
+    )
+    b = widgets.Checkbox(
+        value=False,
+        description=r'Le produit $A \cdot B$ appartient à $M_{3 \times 2}(\mathbb{R})$',
+        disabled=False,
+        layout=Layout(width='80%', height='50px')
+
+    )
+    c = widgets.Checkbox(
+        value=False,
+        description=r'Le produit $A \cdot B$ appartient à $M_{2 \times 1}(\mathbb{R})$',
+        disabled=False,
+        layout=Layout(width='80%', height='50px')
+    )
+    d = widgets.Checkbox(
+        value=False,
+        description=r"$A \cdot B$ n'est pas définie",
+        disabled=False,
+        layout=Layout(width='80%', height='50px')
+    )
+
+    def correction(a, b, c, d):
+        if c and not a and not b and not d:
+            A = [[14], [6]]
+            texA = '$' + texMatrix(A) + '$'
+            display(Latex(r"C'est correct! Le produit $ A \cdot B$ vaut: $A \cdot B$ = " + texA))
+        else:
+            print("C'est faux.")
+
+    interact_manual(correction, a=a, b=b, c=c, d=d)
+
+    return
+
+
+def Ex3Chapitre2_2():
+    """Provides the correction of exercise 3 of notebook 2_2
+    """
+    display(Latex("Insert the values of a and b as floating point numbers"))
+    a = widgets.FloatText(
+        value=0.0,
+        step=0.1,
+        description='a:',
+        disabled=False
+    )
+    b = widgets.FloatText(
+        value=0.0,
+        step=0.1,
+        description='b:',
+        disabled=False
+    )
+
+    display(a)
+    display(b)
+
+    def f():
+        A = np.array([[-1, 2], [5, -2]])
+        B = np.array([[-1, 1], [a.value, b.value]])
+
+        AB = np.dot(A,B)
+        texAB = '$' + texMatrix(AB) + '$'
+        BA = np.dot(B,A)
+        texBA = '$' + texMatrix(BA) + '$'
+
+        if a.value == 5/2 and b.value == -3/2:
+            display(Latex(r"Correcte! Le produits $A \cdot B$ et $B \cdot A$ valent chacun: " + texAB))
+        else:
+            display(Latex(r"Incorrecte! Le produit $A \cdot B$ vaut " + texAB + r"et par contre le produit "
+                          r"$B \cdot A$ vaut " + texBA + r" Entrez de nouvelles valeurs!"))
+
+    interact_manual(f)
+
+    return
+
+
+def Ex1Chapitre2_3(A, B, C):
+    """Provides the correction to exercise 1 of notebook 2_3
+
+    :param A: original matrix
+    :type A: list[list] or numpy.ndarray
+    :param B: matrix such that A+B should be diagonal
+    :type B: list[list] or numpy.ndarray
+    :param C: matrix such that A+C should be symmetric and not diagonal
+    :type C: list[list] or numpy.ndarray
+    :return:
+    :rtype:
+    """
+
+    if not type(A) is np.ndarray:
+        A = np.array(A)
+    if not type(B) is np.ndarray:
+        B = np.array(B)
+    if not type(C) is np.ndarray:
+        C = np.array(C)
+
+    ans1 = isDiag(A+B)
+    ans2 = isSym(A+C) and not isDiag(A+C)
+
+    if ans1 and ans2:
+        print('Correcte!')
+    else:
+        print('Incorrecte! Entrez des nouvelles valeurs pur le matrices B et C!\n')
+
+    if ans1:
+        print("A+B est bien diagonale!")
+    else:
+        print("A+B est n'est pas diagonale!")
+    texAB = '$' + texMatrix(A+B) + '$'
+    display(Latex(r"A+B=" + texAB))
+
+    if ans2:
+        print("A+C est bien symétrique et non diagonale!")
+    elif isSym(A + C) and isDiag(A + C):
+        print("A+C est bien symétrique mais elle est aussi diagonale!")
+    else:
+        print("A + C n'est pas symétrique")
+    texAC = '$' + texMatrix(A + C) + '$'
+    display(Latex(r"A+C=" + texAC))
+
+    return
+
+
+def Ex2Chapitre2_3():
+    """Provides the correction to exercise 2 of notebook 2_3
+    """
+
+    a = widgets.Checkbox(
+        value=False,
+        description=r'$(A^{-1})^T$ et $(A^T)^{-1}$ sont triangulaires supérieures mais différentes',
+        disabled=False,
+        layout=Layout(width='80%', height='40px')
+    )
+    b = widgets.Checkbox(
+        value=False,
+        description=r'$(A^{-1})^T$ et $(A^T)^{-1}$ sont triangulaires inférieures mais différentes',
+        disabled=False,
+        layout=Layout(width='80%', height='40px')
+
+    )
+    c = widgets.Checkbox(
+        value=False,
+        description=r'$(A^{-1})^T$ et $(A^T)^{-1}$ sont triangulaires inférieures et identiques',
+        disabled=False,
+        layout=Layout(width='80%', height='40px')
+    )
+    d = widgets.Checkbox(
+        value=False,
+        description=r'$(A^{-1})^T$ et $(A^T)^{-1}$ sont triangulaires supérieures et identiques',
+        disabled=False,
+        layout=Layout(width='80%', height='40px')
+    )
+
+    def correction(a, b, c, d):
+        if d and not a and not c and not b:
+            A = np.array(([-1, 0, 0], [3, 1/2, 0], [1, 2, 1]))
+            res = np.transpose(np.linalg.inv(A))
+            texAres = '$' + texMatrix(res) + '$'
+            display(Latex("C'est correct! $(A^T)^{-1}$ est donnée par: $\quad$" + texAres))
+        else:
+            display(Latex("C'est faux."))
 
     interact_manual(correction, a=a, b=b, c=c, d=d)
 
